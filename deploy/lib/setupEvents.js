9014fb76e9999e039c8485772c8757128e8d2df2
Jackson Tian <shyvo1987@gmail.com>
2018-04-13 19:59:16 +0800

    refine deploy/lib/setupEvents.js

--- deploy/lib/setupEvents.js
+++ deploy/lib/setupEvents.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const BbPromise = require('bluebird');
 
 module.exports = {
-  setupEvents() {
+  async setupEvents() {
     this.apis = _.filter(
       this.templates.update.Resources,
       (item) => this.provider.isApiType(item.Type))
@@ -15,171 +15,166 @@ module.exports = {
       (item) => this.provider.isTriggerType(item.Type))
       .map((item) => item.Properties);
 
-    return BbPromise.bind(this)
-      .then(this.setupInvokeRole)
-      .then(this.createApisIfNeeded)
-      .then(this.createTriggersIfNeeded);
+    await this.setupInvokeRole();
+    await this.createApisIfNeeded();
+    await this.createTriggersIfNeeded();
   },
 
-  setupInvokeRole() {
+  async setupInvokeRole() {
     const role = this.templates.update.Resources[this.provider.getInvokeRoleLogicalId()].Properties;
-
     // TODO: update if needed
-    return BbPromise.bind(this)
-      .then(() => this.setupRole(role))
-      .then((invokeRole) => this.invokeRole = invokeRole);
+    this.invokeRole = await this.setupRole(role);
   },
 
-  createApisIfNeeded() {
-    if (!this.apis.length) {
-      return Promise.resolve();
-    }
-
-    return BbPromise.bind(this)
-      .then(this.createApiGroupIfNotExists)
-      .then(this.checkExistingApis)
-      .then(this.createOrUpdateApis)
-      .then(this.deployApis);
-  },
-
-  createTriggersIfNeeded() {
-    if (!this.triggers.length) {
-      return Promise.resolve();
-    }
-
-    return BbPromise.bind(this)
-      .then(this.checkExistingTriggers)
-      .then(this.createOrUpdateTriggers);
-  },
-
-  createApiGroupIfNotExists() {
-    const groupResource = this.templates.update.Resources[this.provider.getApiGroupLogicalId()];
-
-    if (!groupResource) {
-      return Promise.resolve();  // No API needed
-    }
-    const group = groupResource.Properties;
-
-    const groupName = group.GroupName;
-
-    return this.provider.getApiGroup(groupName)
-      .then((foundGroup) => {
-        if (foundGroup) {
-          this.apiGroup = foundGroup;
-          this.serverless.cli.log(`API group ${group.GroupName} exists.`);
-          return foundGroup;
-        }
-        return this.createApiGroup(group);
-      });
-  },
-
-  createApiGroup(group) {
-    this.serverless.cli.log(`Creating API group ${group.GroupName}...`);
-    return this.provider.createApiGroup(group)
-      .then((createdGroup) => {
-        this.apiGroup = createdGroup;
-        this.serverless.cli.log(`Created API group ${group.GroupName}`);
-        return createdGroup;
-      });
-  },
-
-  checkExistingApis() {
+  async createApisIfNeeded() {
     if (!this.apis.length) {
       return;
     }
 
-    return this.provider.getApis({
+    await this.createApiGroupIfNotExists();
+    await this.checkExistingApis();
+    await this.createOrUpdateApis();
+    await this.deployApis();
+  },
+
+  async createTriggersIfNeeded() {
+    if (!this.triggers.length) {
+      return;
+    }
+
+    await this.checkExistingTriggers();
+    await this.createOrUpdateTriggers();
+  },
+
+  async createApiGroupIfNotExists() {
+    const groupResource = this.templates.update.Resources[this.provider.getApiGroupLogicalId()];
+    if (!groupResource) {
+      return; // No API needed
+    }
+
+    const group = groupResource.Properties;
+    const groupName = group.GroupName;
+
+    const foundGroup = await this.provider.getApiGroup(groupName);
+    if (foundGroup) {
+      this.apiGroup = foundGroup;
+      this.serverless.cli.log(`API group ${group.GroupName} exists.`);
+      return foundGroup;
+    }
+
+    await this.createApiGroup(group);
+  },
+
+  async createApiGroup(group) {
+    this.serverless.cli.log(`Creating API group ${group.GroupName}...`);
+    const createdGroup = await this.provider.createApiGroup(group);
+    this.apiGroup = createdGroup;
+    this.serverless.cli.log(`Created API group ${group.GroupName}`);
+    return createdGroup;
+  },
+
+  async checkExistingApis() {
+    if (!this.apis.length) {
+      return;
+    }
+
+    const apis = await this.provider.getApis({
       GroupId: this.apiGroup.GroupId
-    }).then((apis) => {
-      this.apiMap = new Map(apis.map((api) => [api.ApiName, api]));
-      this.apis.forEach((api) => {
-        if (!this.apiMap.get(api.ApiName)) {
-          this.apiMap.set(api.ApiName, false);
-        }
-      });
+    });
+    this.apiMap = new Map(apis.map((api) => [api.ApiName, api]));
+    this.apis.forEach((api) => {
+      if (!this.apiMap.get(api.ApiName)) {
+        this.apiMap.set(api.ApiName, false);
+      }
     });
   },
 
-  createOrUpdateApis(group) {
+  async createOrUpdateApis() {
     if (!this.apis.length) {
       return;
     }
 
-    return BbPromise.mapSeries(this.apis,
-      (api) => this.createOrUpdateApi(api));
+    for (var i = 0; i < this.apis.length; i++) {
+      const api = this.apis[i];
+      await this.createOrUpdateApi(api);
+    }
   },
 
-  createOrUpdateApi(api) {
+  async createOrUpdateApi(api) {
     const group = this.apiGroup;
     const role = this.invokeRole;
     const apiInMap = this.apiMap.get(api.ApiName);
     if (apiInMap) {
       const apiProps = Object.assign({ApiId: apiInMap.ApiId}, api);
       this.serverless.cli.log(`Updating API ${api.ApiName}...`);
-      return this.provider.updateApi(group, role, apiProps)
-        .then((newApi) => {
-          this.serverless.cli.log(`Updated API ${api.ApiName}`);
-        }, (err) => {
-          this.serverless.cli.log(`Failed to update API ${api.ApiName}!`);
-          throw err;
-        });
-    }
-    this.serverless.cli.log(`Creating API ${api.ApiName}...`);
-    return this.provider.createApi(group, role, api)
-      .then((newApi) => {
-        this.serverless.cli.log(`Created API ${api.ApiName}`);
-        this.apiMap.set(api.ApiName, newApi);
-      }, (err) => {
-        this.serverless.cli.log(`Failed to create API ${api.ApiName}!`);
+      try {
+        await this.provider.updateApi(group, role, apiProps);
+        this.serverless.cli.log(`Updated API ${api.ApiName}`);
+      } catch (err) {
+        this.serverless.cli.log(`Failed to update API ${api.ApiName}!`);
         throw err;
-      });
+      }
+      return;
+    }
 
+    this.serverless.cli.log(`Creating API ${api.ApiName}...`);
+    let newApi;
+    try {
+      newApi = await this.provider.createApi(group, role, api);
+    } catch (err) {
+      this.serverless.cli.log(`Failed to create API ${api.ApiName}!`);
+      throw err;
+    }
+    this.serverless.cli.log(`Created API ${api.ApiName}`);
+    this.apiMap.set(api.ApiName, newApi);
   },
 
-  deployApis() {
+  async deployApis() {
     const group = this.apiGroup;
-    return BbPromise.mapSeries(this.apis, (api) => {
+    for (var i = 0; i < this.apis.length; i++) {
+      const api = this.apis[i];
       const apiProps = this.apiMap.get(api.ApiName);
       this.serverless.cli.log(`Deploying API ${api.ApiName}...`);
-      return this.provider.deployApi(group, apiProps).then(
-        () => {
-          this.serverless.cli.log(`Deployed API ${api.ApiName}`);
-          const config = api.RequestConfig;
-          const func = api.ServiceConfig.FunctionComputeConfig;
-          this.serverless.cli.log(`${config.RequestHttpMethod} ` +
-          `http://${this.apiGroup.SubDomain}${config.RequestPath} -> ` +
-          `${func.ServiceName}.${func.FunctionName}`);
-        },
-        (err) => {
-          this.serverless.cli.log(`Failed to deploy API ${api.ApiName}!`);
-          throw err;
-        });
-    });
+      try {
+        await this.provider.deployApi(group, apiProps);
+        this.serverless.cli.log(`Deployed API ${api.ApiName}`);
+        const config = api.RequestConfig;
+        const func = api.ServiceConfig.FunctionComputeConfig;
+        this.serverless.cli.log(`${config.RequestHttpMethod} ` +
+        `http://${this.apiGroup.SubDomain}${config.RequestPath} -> ` +
+        `${func.ServiceName}.${func.FunctionName}`);
+      } catch (err) {
+        this.serverless.cli.log(`Failed to deploy API ${api.ApiName}!`);
+        throw err;
+      }
+    }
   },
 
-  checkExistingTriggers() {
+  async checkExistingTriggers() {
     this.triggerMap = new Map();
-    return BbPromise.mapSeries(this.triggers, (trigger) => {
-      return this.provider.getTrigger(
+    for (var i = 0; i < this.triggers.length; i++) {
+      const trigger = this.triggers[i];
+      const foundTrigger = await this.provider.getTrigger(
         trigger.serviceName, trigger.functionName, trigger.triggerName
-      ).then((foundTrigger) => {
-        if (foundTrigger) {
-          this.triggerMap.set(trigger.triggerName, foundTrigger);
-        }
-      });
-    });
+      );
+      if (foundTrigger) {
+        this.triggerMap.set(trigger.triggerName, foundTrigger);
+      }
+    }
   },
 
-  createOrUpdateTriggers() {
+  async createOrUpdateTriggers() {
     if (!this.triggers.length) {
       return;
     }
 
-    return BbPromise.mapSeries(this.triggers,
-      (trigger) => this.createOrUpdateTrigger(trigger));
+    for (var i = 0; i < this.triggers.length; i++) {
+      const trigger = this.triggers[i];
+      await this.createOrUpdateTrigger(trigger);
+    }
   },
 
-  createOrUpdateTrigger(trigger) {
+  async createOrUpdateTrigger(trigger) {
     const role = this.invokeRole;
     const triggerName = trigger.triggerName;
     const serviceName = trigger.serviceName;
@@ -187,23 +182,24 @@ module.exports = {
     const triggerInMap = this.triggerMap.get(triggerName);
     if (triggerInMap) {
       this.serverless.cli.log(`Updating trigger ${triggerName}...`);
-      return this.provider.updateTrigger(serviceName, functionName, triggerName, trigger, role)
-        .then((newtrigger) => {
-          this.serverless.cli.log(`Updated trigger ${triggerName}`);
-        }, (err) => {
-          this.serverless.cli.log(`Failed to update trigger ${triggerName}!`);
-          throw err;
-        });
-    }
-    this.serverless.cli.log(`Creating trigger ${triggerName}...`);
-    return this.provider.createTrigger(serviceName, functionName, trigger, role)
-      .then((newtrigger) => {
-        this.serverless.cli.log(`Created trigger ${triggerName}`);
-        this.triggerMap.set(triggerName, newtrigger);
-      }, (err) => {
-        this.serverless.cli.log(`Failed to create trigger ${triggerName}!`);
+      try {
+        await this.provider.updateTrigger(serviceName, functionName, triggerName, trigger, role);
+        this.serverless.cli.log(`Updated trigger ${triggerName}`);
+      } catch (err) {
+        this.serverless.cli.log(`Failed to update trigger ${triggerName}!`);
         throw err;
-      });
+      }
+      return;
+    }
 
+    this.serverless.cli.log(`Creating trigger ${triggerName}...`);
+    try {
+      const newtrigger = await this.provider.createTrigger(serviceName, functionName, trigger, role);
+      this.serverless.cli.log(`Created trigger ${triggerName}`);
+      this.triggerMap.set(triggerName, newtrigger);
+    } catch (err) {
+      this.serverless.cli.log(`Failed to create trigger ${triggerName}!`);
+      throw err;
+    }
   }
 };
