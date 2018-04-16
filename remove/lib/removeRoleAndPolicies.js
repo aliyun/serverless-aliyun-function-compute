9f52a5b5481a13f72cbed6dca513fe3fe6e0ab97
Jackson Tian <shyvo1987@gmail.com>
2018-04-17 00:26:11 +0800

    improve shared/validate.js

--- remove/lib/removeRoleAndPolicies.js
+++ remove/lib/removeRoleAndPolicies.js
@@ -1,58 +1,50 @@
 'use strict';
 
-const BbPromise = require('bluebird');
-
 module.exports = {
-  removeRoleAndPolicies(roleName) {
+  async removeRoleAndPolicies(roleName) {
     if (!this.options['remove-roles']) {
-      return Promise.resolve();
+      return;
     }
     const roleSym = Symbol(roleName);
     const policySym = Symbol(roleName + '-policies');
     this[roleSym] = undefined;
     this[policySym] = [];
 
-    return this.getRamInfo(roleName, roleSym, policySym)
-      .then(() => this.removePolicyIfExists(roleName, roleSym, policySym))
-      .then(() => this.removeRoleIfExists(roleName, roleSym, policySym));
+    await this.getRamInfo(roleName, roleSym, policySym);
+    await this.removePolicyIfExists(roleName, roleSym, policySym);
+    await this.removeRoleIfExists(roleName, roleSym, policySym);
   },
 
-  getRamInfo(roleName, roleSym, policySym) {
-    return this.provider.getRole(roleName)
-      .then((foundRole) => {
-        if (!foundRole) { return; }
-        this[roleSym] = foundRole;
-        return this.provider.getPoliciesForRole(roleName)
-          .then((policies) => {
-            this[policySym] = policies;
-          });
-      });
+  async getRamInfo(roleName, roleSym, policySym) {
+    const foundRole = await this.provider.getRole(roleName);
+    if (!foundRole) {
+      return;
+    }
+    this[roleSym] = foundRole;
+    this[policySym] = await this.provider.getPoliciesForRole(roleName);
   },
 
-  removePolicyIfExists(roleName, roleSym, policySym) {
+  async removePolicyIfExists(roleName, roleSym, policySym) {
     if (!this[roleSym] || !this[policySym].length) {
       return;
     }
     const role = this[roleSym];
     const policies = this[policySym];
-    return BbPromise.map(policies, (policyProps) => {
+    for (var i = 0; i < policies.length; i++) {
+      const policyProps = policies[i];
       const policyName = policyProps.PolicyName;
       this.serverless.cli.log(`Detaching RAM policy ${policyName} from ${roleName}...`);
-      return this.provider.detachPolicyFromRole(role, policyProps).then(() => {
-        this.serverless.cli.log(`Detached RAM policy ${policyName} from ${roleName}`);
-        return;
-      });
-    });
+      await this.provider.detachPolicyFromRole(role, policyProps);
+      this.serverless.cli.log(`Detached RAM policy ${policyName} from ${roleName}`);
+    }
   },
 
-  removeRoleIfExists(roleName, roleSym, policySym) {
+  async removeRoleIfExists(roleName, roleSym, policySym) {
     if (!this[roleSym]) {
       return;
     }
     this.serverless.cli.log(`Removing RAM role ${roleName}...`);
-    return this.provider.deleteRole(roleName)
-      .then(() => {
-        this.serverless.cli.log(`Removed RAM role ${roleName}`);
-      });
+    await this.provider.deleteRole(roleName);
+    this.serverless.cli.log(`Removed RAM role ${roleName}`);
   }
 };
