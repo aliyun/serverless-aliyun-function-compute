41da9fd220f78210b3973b64853f951543af73dd
倚贤 <yixian.dw@alibaba-inc.com>
2019-09-08 11:02:31 +0800

    update index.js require

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const AliyunProvider = require('../provider/aliyunProvider');
-const AliyunPackage = require('../package/aliyunPackage');
+const AliyunProvider = require('./provider/aliyunProvider');
+const AliyunPackage = require('./package/aliyunPackage');
 const AliyunDeploy = require('./deploy/aliyunDeploy');
 const AliyunDeployFunction = require('./deploy/aliyunDeployFunction');
-const AliyunRemove = require('../remove/aliyunRemove');
+const AliyunRemove = require('./remove/aliyunRemove');
 const AliyunInvoke = require('./invoke/aliyunInvoke');
 const AliyunLogs = require('./logs/aliyunLogs');
 const AliyunInfo = require('./info/aliyunInfo');
